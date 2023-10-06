15c7e835dcdcffa2922037fa49d74f8d9f598a9c
Neville Brem <contact@nevillebrem.com>
2023-10-06 08:33:01 +0200

    finished header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   return (
-    <header className='flex justify-between p-4 bg-bmOrange'>
+    <header className='flex justify-between p-4 bg-bmOrange uppercase font-black font-[Montserrat]'>
       <a
         href='/'
         className='w-24 hover:rotate-12 hover:scale-110 ease-in-out duration-100'
       >
         <img src={BarSVG} alt='' />
       </a>
-      <div className='font-bold text-2xl my-auto text-bmPink1'>
-        <a href=''>About us</a>
+      <div className='text-2xl my-auto text-bmPink1 flex gap-4'>
+        <a
+          className='hover:text-bmPink2 hover:scale-110 ease-in-out duration-200'
+          href='/about'
+        >
+          About us
+        </a>
+        <a
+          className='hover:text-bmPink2 hover:scale-110 ease-in-out duration-200'
+          href='/mission'
+        >
+          Our Mission
+        </a>
       </div>
       <FontAwesomeIcon
-        className='text-bmBlue1 text-3xl my-auto'
+        className='text-bmBlue1 hover:text-bmBlue2 text-3xl my-auto hover:scale-110 ease-in-out duration-100 cursor-pointer'
         icon={faUser}
       />
     </header>
