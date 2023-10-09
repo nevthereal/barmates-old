bf19d57d08feb2a374b5df132558317cd79b89b2
Neville Brem <contact@nevillebrem.com>
2023-10-09 10:33:11 +0200

    typing animation on Hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import Typed from "typed.js";
+import { useRef, useEffect } from "react";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -10,6 +12,21 @@ const Home = () => {
 };
 
 const Hero = () => {
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Innovative", "Delicious", "Saturating", "Protein-Rich"],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true,
+      loopCount: 20,
+    });
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <div className='h-[70dvh] hero flex flex-col text-center'>
       <div className='mx-auto my-auto'>
@@ -17,15 +34,19 @@ const Hero = () => {
           BarMates
         </h1>
         <p className='text-bmBlue1 font-bold text-base md:text-3xl'>
-          Innovative bars made in Switzerland
+          <span className='font-extrabold' ref={el}></span> bars made in
+          Switzerland
         </p>
       </div>
-      <a href='#about' className='text-bmBlue1 mb-8 group mx-auto'>
+      <a
+        href='#about'
+        className='text-bmBlue1 hover:text-bmBlue2 mb-8 group mx-auto'
+      >
         <p className='md:text-xl font-bold group-hover:text-bmBlue2 duration-200'>
           Find out more!
         </p>
         <FontAwesomeIcon
-          className='text-2xl md:text-4xl group-hover:translate-y-2 group-hover:text-bmBlue2 duration-200 ease-in-out'
+          className='text-2xl md:text-4xl group-hover:translate-y-2 duration-200 ease-in-out'
           icon={faChevronDown}
         />
       </a>
