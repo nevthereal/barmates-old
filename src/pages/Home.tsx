7b4d8a8d2df9b0cba4a7d8ac3777b5e8d809cc6c
Neville Brem <contact@nevillebrem.com>
2023-10-10 10:28:04 +0200

    added about section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { motion } from "framer-motion";
+import Founders from "../assets/FOUNDERS.jpeg";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 };
@@ -48,7 +50,31 @@ const Hero = () => {
 };
 
 const About = () => {
-  return <></>;
+  return (
+    <div
+      className='grid grid-flow-row md:grid-flow-col text-bmBlue1'
+      id='about'
+    >
+      <div className='flex flex-col justify-center p-2 md:p-8'>
+        <h1 className='mx-auto my-2 text-2xl md:text-4xl font-black font-[Montserrat]'>
+          The Founders
+        </h1>
+        <img
+          src={Founders}
+          alt='founders'
+          className='rounded-2xl w-[50%] mx-auto'
+        />
+      </div>
+      <div className='p-8 flex'>
+        <p className='my-auto text-lg md:text-xl'>
+          We (Marko and Neville) are two 16 year old students from Zurich,
+          Switzerland. We are rowing athletes at the Belvoir Rowing Club Zurich.
+          We love protein and energy bars, but we figured, there is no perfect
+          one. In 2023 we came up with the idea to create our own bars.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
