b0e3c96c36292c7ef4b88126b0e147e91d73eed4
Neville Brem <contact@nevillebrem.com>
2023-10-30 07:05:14 +0100

    fixed issue, added some new files for subpages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Catalogue from "./pages/Catalogue";
+import Mission from "./pages/Mission";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <Routes>
         <Route index element={<Home />} />
+        <Route path='/catalogue' element={<Catalogue />} />
+        <Route path='/mission' element={<Mission />} />
       </Routes>
       <Footer />
     </>
